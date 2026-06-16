03de7fb44f9dbaae36c3b9ff173eabab58a2429e
17214 <1721476339@qq.com>
2026-06-16 17:25:07 +0800

    feat(): 修复合并bug

--- src/views/student/MyScores.vue
+++ src/views/student/MyScores.vue
@@ -12,12 +12,7 @@
             <el-tag size="small" type="primary">{{ row.subjectName || '科目' + row.subject }}</el-tag>
           </template>
         </el-table-column>
-        <el-table-column label="车型" width="70" align="center">
-          <template #default="{ row }">
-            <span>{{ row.licenseType || '-' }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column label="考试日期" width="120" align="center">
+<el-table-column label="考试日期" width="120" align="center">
           <template #default="{ row }">
             {{ row.examDate || '-' }}
           </template>
